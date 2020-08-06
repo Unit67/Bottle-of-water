24ac362f8f899b5bb06308738ce92813081c0f35
Unit67 <Unit67@interia.pl>
2020-08-06 18:38:39 +0200

    First Commit

---
+++ Assets/_Code/_HumanController/HumanController.cs
@@ -0,0 +1,28 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class HumanController : NetworkBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(isLocalPlayer)
+        {
+            _animator.SetFloat("Walk", Input.GetAxis("Vertical"));
+            _animator.SetFloat("HorizontalWalk", Input.GetAxis("Horizontal"));
+
+            if(Input.GetAxis("Vertical") > 0.1 & Input.GetKey(KeyCode.LeftShift))
+            {
+                _animator.SetFloat("Walk", 3);
+            }
+        }
+    }
+}
