8ccb9d46563a1e8962739e333d4fcdfa122ee22b
Unit67 <Unit67@interia.pl>
2020-08-09 03:19:06 +0200

    Bottle controller complete

---
+++ Assets/_Code/_Bottle/_ControlerManager/ControllerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerManager : MonoBehaviour
+{
+    public KeyCode SprintKey = KeyCode.LeftShift;
+
+    public bool GetButtonState(KeyCode keyCode)
+    {
+        if(Input.GetKey(keyCode))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- Assets/_Code/_Bottle/_Movement/BottleControllerNoAnim.cs
+++ Assets/_Code/_Bottle/_Movement/BottleControllerNoAnim.cs
@@ -1,28 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BottleControllerNoAnim : MonoBehaviour
+using Mirror;
+public class BottleControllerNoAnim : NetworkBehaviour
 {
     private CharacterController _Chc;
     private Vector3 _VMotion;
 
-    public float MoveSpeed = 5.0f;
+    public float WalkSpeed = 5.0f;
+    public float SprintSpeed = 10.0f;
+    private float _CurrentSpeed;
+
+    private float _Grav;
 
     public GameObject CameraContainer;
+    public ControllerManager controllerManager;
+
+    public GameObject ControllerManager;
     private void Start()
     {
         _Chc = gameObject.GetComponent<CharacterController>();
+        GameObject.Find("ControllerManager").GetComponent<ControllerManager>();
     }
 
     private void Update()
     {
-        Vector3 movementX = CameraContainer.transform.right * Input.GetAxis("Horizontal");
-        Vector3 movementZ = CameraContainer.transform.forward * Input.GetAxis("Vertical");
-        Vector3 movement = movementX + movementZ;
 
-        _VMotion = new Vector3(-movement.x, 0, -movement.z);
+        if(isLocalPlayer)
+        {
+            //sprint
+            if(controllerManager != null)
+            {
+                if(Input.GetAxis("Vertical") > 0.1f & controllerManager.GetButtonState(controllerManager.SprintKey))
+                {
+                    _CurrentSpeed = SprintSpeed;
+                }
+                else
+                {
+                    _CurrentSpeed = WalkSpeed;
+                }
+            }
+            else
+            {
+                Debug.Log("controllerManager == null");
+                ControllerManager = GameObject.Find("ControllerManager");
+                controllerManager = ControllerManager.GetComponent<ControllerManager>();
+            }
 
-        _Chc.Move(_VMotion * MoveSpeed * Time.deltaTime);
+
+            //grav calculation
+            _Grav = 5 * 10 * Time.deltaTime;
+
+            //movement
+            Vector3 movementX = CameraContainer.transform.right * Input.GetAxis("Horizontal");
+            Vector3 movementZ = CameraContainer.transform.forward * Input.GetAxis("Vertical");
+            Vector3 movement = movementX + movementZ;
+            _VMotion = new Vector3(-movement.x, -_Grav, -movement.z);
+            _Chc.Move(_VMotion * _CurrentSpeed * Time.deltaTime);
+        }
+        else
+        {
+            CameraContainer.SetActive(false);
+            this.gameObject.GetComponent<BottleControllerNoAnim>().enabled = false;
+        }
+
     }
 }
